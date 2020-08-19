91adf488859b3eb318795468fb858fc0e912fce3
jerichocalibuso <jerichocalibuso@gmail.com>
2020-08-19 15:32:44 +0800

    Updates

---
+++ precache-manifest.72c4b3b1fb6ad2cd18cef75b82760c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4954a0f3b5fbca80311c6e5eefb797ea",
+    "url": "/jerichocalibuso/todo-list/index.html"
+  },
+  {
+    "revision": "a6213a4a68bf2f12ce99",
+    "url": "/jerichocalibuso/todo-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a27e2247808ca90c0dcb",
+    "url": "/jerichocalibuso/todo-list/static/js/2.fc761930.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/jerichocalibuso/todo-list/static/js/2.fc761930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6213a4a68bf2f12ce99",
+    "url": "/jerichocalibuso/todo-list/static/js/main.ea9e7474.chunk.js"
+  },
+  {
+    "revision": "446612e652392803a0a9",
+    "url": "/jerichocalibuso/todo-list/static/js/runtime-main.c413e709.js"
+  }
+]);
